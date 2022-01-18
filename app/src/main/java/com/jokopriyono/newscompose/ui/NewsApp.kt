921caba1c8da4f6d6f572feb32716b738cc4dad7
jokopriyono <joe.mandrake007@gmail.com>
2022-01-18 16:25:23 +0700

    adding Bottom Navigation and implement with navHost

--- app/src/main/java/com/jokopriyono/newscompose/ui/NewsApp.kt
+++ app/src/main/java/com/jokopriyono/newscompose/ui/NewsApp.kt
@@ -1,41 +1,61 @@
 package com.jokopriyono.newscompose.ui
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import com.jokopriyono.newscompose.BottomMenuScreen
 import com.jokopriyono.newscompose.MockData
+import com.jokopriyono.newscompose.components.BottomMenu
+import com.jokopriyono.newscompose.ui.screen.Categories
 import com.jokopriyono.newscompose.ui.screen.DetailScreen
+import com.jokopriyono.newscompose.ui.screen.Sources
 import com.jokopriyono.newscompose.ui.screen.TopNews
 
 @Composable
 fun NewsApp() {
-    Navigation()
+    val scrollState = rememberScrollState()
+    val navController = rememberNavController()
+    MainScreen(navController = navController, scrollState)
 }
 
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
-    val scrollState = rememberScrollState()
+fun MainScreen(navController: NavHostController, scrollState: ScrollState) {
+    Scaffold(bottomBar = {
+        BottomMenu(navController = navController)
+    }) {
+        Navigation(navController = navController, scrollState = scrollState)
+    }
+}
 
-    NavHost(navController = navController, startDestination = "TopNews") {
-        composable("TopNews") {
-            TopNews(navController = navController)
-        }
-        composable(
-            "Detail/{newsId}",
-            arguments = listOf(navArgument("newsId") { type = NavType.IntType })
-        ) {
-            val id = it.arguments?.getInt("newsId")
+
+@Composable
+fun Navigation(navController: NavHostController, scrollState: ScrollState) {
+    NavHost(navController = navController, startDestination = BottomMenuScreen.TopNews.route) {
+        bottomNavigation(navController = navController)
+        composable("Detail/{newsId}",
+            arguments = listOf(
+                navArgument("newsId") { type = NavType.IntType }
+            )) { navBackStackEntry ->
+            val id = navBackStackEntry.arguments?.getInt("newsId")
             val newsData = MockData.getNews(id)
-            DetailScreen(
-                newsData = newsData,
-                scrollState = scrollState,
-                navController = navController
-            )
+            DetailScreen(newsData, scrollState, navController)
         }
+    }
+}
+
+fun NavGraphBuilder.bottomNavigation(navController: NavController) {
+    composable(BottomMenuScreen.TopNews.route) {
+        TopNews(navController = navController)
+    }
+    composable(BottomMenuScreen.Categories.route) {
+        Categories()
+    }
+    composable(BottomMenuScreen.Sources.route) {
+        Sources()
     }
 }
