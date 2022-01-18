473e8250407bc4af08399f29ac914b9cebc113b5
jokopriyono <joe.mandrake007@gmail.com>
2022-01-18 10:26:28 +0700

    show all news detail data

--- app/src/main/java/com/jokopriyono/newscompose/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/jokopriyono/newscompose/ui/screen/DetailScreen.kt
@@ -1,36 +1,71 @@
 package com.jokopriyono.newscompose.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.dp
 import com.jokopriyono.newscompose.MockData
 import com.jokopriyono.newscompose.NewsData
+import com.jokopriyono.newscompose.R
 
 @Composable
-fun DetailScreen(navController: NavController, newsData: NewsData) {
+fun DetailScreen(newsData: NewsData, scrollState: ScrollState) {
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Detail Screen", fontWeight = FontWeight.SemiBold)
-        Button(onClick = {
-            navController.popBackStack()
-        }) {
-            Text(text = "Go to Top News Screen + ${newsData.author}")
+        Image(painter = painterResource(id = newsData.image), contentDescription = "")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            InfoWithIcon(icon = Icons.Default.Edit, info = newsData.author)
+            InfoWithIcon(icon = Icons.Default.DateRange, info = newsData.publishedAt)
         }
+
+        Text(text = newsData.title, fontWeight = FontWeight.Bold)
+        Text(text = newsData.description, modifier = Modifier.padding(top = 16.dp))
+    }
+}
+
+@Composable
+fun InfoWithIcon(icon: ImageVector, info: String) {
+    Row {
+        Icon(
+            imageVector = icon,
+            contentDescription = info,
+            modifier = Modifier.padding(end = 8.dp),
+            colorResource(
+                id = R.color.purple_500
+            )
+        )
+        Text(text = info)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DetailScreenPreview() {
-    DetailScreen(rememberNavController(), MockData.topNewsList[0])
+    DetailScreen(MockData.topNewsList[0], rememberScrollState())
 }
